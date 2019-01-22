757abaca8fd1472d54a7d12b87801462b28b8f55
edunga1 <goonr21@gmail.com>
2019-01-22 11:02:58 +0900

    Updates

---
+++ precache-manifest.0879e9e1a0e0c068d5867f5b22919dc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05589afe5714d39c8ebb",
+    "url": "/static/js/main.05589afe.chunk.js"
+  },
+  {
+    "revision": "80e36f38591b6f91c7eb",
+    "url": "/static/js/1.80e36f38.chunk.js"
+  },
+  {
+    "revision": "05589afe5714d39c8ebb",
+    "url": "/static/css/main.9cb2c72d.chunk.css"
+  },
+  {
+    "revision": "79ba0317dc13647ccfdbcd7092a7c202",
+    "url": "/index.html"
+  }
+];
